d6125927c0deb7d6ac6dd41017a7f26699777b4e
khubaib321 <khubaib.afzal11@gmail.com>
2025-06-15 05:54:05 +0200

    Add config; move scanner inside

--- main.py
+++ main.py
@@ -1,141 +1,16 @@
-import os as _os
-import pathlib as _pathlib
-import json as _json
-import threading as _threading
-from queue import Queue, Empty
-
-import helpers as _helpers
-
-_MAX_WORKERS = 48
-_IGNORE_DIRS = {
-    # "tmp",
-    # "bin", 
-    # "sbin", 
-    # "venv", 
-    # "dist",
-    # "Volumes",
-    # "__pycache__",
-    # "virtualenvs",
-    # "node_modules",
-}
-_IGNORE_HIDDEN_DIRS = True
-_IGNORE_HIDDEN_FILES = True
-
-
-def _should_ignore_dir(name: str) -> bool:
-    return name in _IGNORE_DIRS or (name.startswith('.') and _IGNORE_HIDDEN_DIRS)
-
-
-def _should_ignore_file(name: str) -> bool:
-    return name.startswith(".") and _IGNORE_HIDDEN_FILES
-
-
-def _worker(work_q: Queue):
-    while True:
-        try:
-            path, bucket = work_q.get(timeout=3)
-        except Empty:
-            continue
-
-        if path is None:
-            break
-
-        bucket["__files__"] = []
-
-        try:
-            with _os.scandir(path) as it:
-                for entry in it:
-                    if (
-                        entry.is_file(follow_symlinks=False)
-                        and not _should_ignore_file(entry.name)
-                    ):
-                        bucket["__files__"].append(entry.name)
-
-                    elif (
-                        entry.is_dir(follow_symlinks=False)
-                        and not _should_ignore_dir(entry.name)
-                    ):
-
-                        sub_bucket = {}
-                        bucket[entry.name] = sub_bucket
-                        work_q.put((entry.path, sub_bucket))
-
-        except OSError as e:
-            bucket["__error__"] = str(e)
-
-        finally:
-            work_q.task_done()
-
-
-@_helpers.time_it()
-def scan_directory(root_dir: str) -> dict:
-    root_path = _pathlib.Path(root_dir).expanduser()
-    if not (root_path.exists() and root_path.is_dir()):
-        return {}
-
-    work_q: Queue = Queue()
-    result: dict = {}
-    work_q.put((str(root_path), result))
-
-    threads = [
-        _threading.Thread(target=_worker, args=(work_q,), daemon=True)
-        for _ in range(_MAX_WORKERS)
-    ]
-    for t in threads:
-        t.start()
-
-    work_q.join()
-
-    # trigger kill switch so the workers exit
-    for _ in threads:
-        work_q.put((None, None))
-    for t in threads:
-        t.join()
-
-    return result
-
-
-def _summarize(*, scan_result: dict) -> tuple[int, int, int]:
-    if "__error__" in  scan_result:
-        return 1, 0, 0
-
-    error = 0
-    dir_count = len(scan_result) - 1
-    file_count = len(scan_result["__files__"])
-
-    for key, value in scan_result.items():
-        if key != "__files__":
-            ret = _summarize(scan_result=value)
-            error += ret[0]
-            dir_count += ret[1]
-            file_count += ret[2]
-
-    return error, dir_count, file_count
+import src.scanner as _scanner
 
 
 def main():
-    root = _os.path.expanduser("/")
-    scan_result = scan_directory(root)
-
-    print("✍️ Writing to file... ", end="", flush=True)
-    _os.makedirs("outputs", exist_ok=True)
-    with open("outputs/file_structure.json", "w") as fh:
-        _json.dump(scan_result, fh, indent=4)
-    print("✅")
-
-    print("+ Summarizing... ", end="", flush=True)
-    errors, total_dirs, total_files = _summarize(scan_result=scan_result)
-    print("✅")
-
-    print("-------------- Summary --------------")
-    print(f"Scanned: '{str(root)}'")
-    print(f"Workers used: {_MAX_WORKERS}")
-    print("Hidden dirs considered:", "❌" * _IGNORE_HIDDEN_DIRS or "✅")
-    print("Hidden files considered:", "❌" * _IGNORE_HIDDEN_FILES or "✅")
-    print(f"Total dirs: {total_dirs:,}")
-    print(f"Total files: {total_files:,}")
-    print(f"Failed scans: {errors:,}")
-    print("-------------- Summary --------------")
+    _scanner.start(
+        dir="~",
+        config={
+            "summarize": True,
+            "max_workers": 24,
+            "scan_hidden_dirs": True,
+            "scan_hidden_files": True,
+        },
+    )
 
 
 if __name__ == "__main__":
---
+++ src/scanner.py
@@ -0,0 +1,171 @@
+import os as _os
+import pathlib as _pathlib
+import json as _json
+from re import sub
+import threading as _threading
+import queue as _q
+from typing import Any
+
+from . import _helpers
+
+_MAX_WORKERS = 48
+_IGNORE_DIRS = {
+    # "tmp",
+    # "bin", 
+    # "sbin", 
+    # "venv", 
+    # "dist",
+    # "Volumes",
+    # "__pycache__",
+    # "virtualenvs",
+    # "node_modules",
+}
+_SCAN_HIDDEN_DIRS = True
+_SCAN_HIDDEN_FILES = True
+
+
+def _should_ignore_dir(path: str, name: str, scan_hidden: bool) -> bool:
+    return name in _IGNORE_DIRS or (not scan_hidden and name.startswith('.'))
+
+
+def _should_ignore_file(name: str, scan_hidden: bool) -> bool:
+    return not scan_hidden and name.startswith(".")
+
+
+def _worker(work_q: _q.Queue):
+    while True:
+        try:
+            params = work_q.get(timeout=3)
+        except _q.Empty:
+            continue
+
+        path = params["path"]
+        if not path:
+            break
+
+        params["bucket"]["__files__"] = []
+        scan_hidden_dirs = params["scan_hidden_dirs"]
+        scan_hidden_files = params["scan_hidden_files"]
+
+        try:
+            with _os.scandir(path) as it:
+                for entry in it:
+                    if (
+                        entry.is_file(follow_symlinks=False)
+                        and not _should_ignore_file(entry.name, scan_hidden_files)
+                    ):
+                        params["bucket"]["__files__"].append(entry.name)
+
+                    elif (
+                        entry.is_dir(follow_symlinks=False)
+                        and not _should_ignore_dir(entry.path, entry.name, scan_hidden_dirs)
+                    ):
+
+                        sub_bucket = {}
+                        params["bucket"][entry.name] = sub_bucket
+                        work_q.put({
+                            "path": entry.path,
+                            "bucket": sub_bucket,
+                            "scan_hidden_dirs": scan_hidden_dirs,
+                            "scan_hidden_files": scan_hidden_files,
+                        })
+
+        except OSError as e:
+            params["bucket"]["__error__"] = str(e)
+
+        finally:
+            work_q.task_done()
+
+
+@_helpers.time_it()
+def _scan_directory(
+    *, 
+    root_dir: str, 
+    max_workers: int, 
+    scan_hidden_dirs: bool, 
+    scan_hidden_files: bool
+) -> dict:
+    root_path = _pathlib.Path(root_dir).expanduser()
+    if not (root_path.exists() and root_path.is_dir()):
+        return {}
+
+    work_q = _q.Queue()
+    result: dict = {}
+    work_q.put(
+        {
+            "path": str(root_path),
+            "bucket": result,
+            "scan_hidden_dirs": scan_hidden_dirs,
+            "scan_hidden_files": scan_hidden_files,
+        },
+    )
+
+    threads = [
+        _threading.Thread(target=_worker, args=(work_q,), daemon=True)
+        for _ in range(max_workers)
+    ]
+    for t in threads:
+        t.start()
+
+    work_q.join()
+
+    # trigger kill switch so the workers exit
+    for _ in threads:
+        work_q.put({"path": None})
+    for t in threads:
+        t.join()
+
+    return result
+
+
+def _summarize(*, scan_result: dict) -> tuple[int, int, int]:
+    if "__error__" in  scan_result:
+        return 1, 0, 0
+
+    error = 0
+    dir_count = len(scan_result) - 1
+    file_count = len(scan_result["__files__"])
+
+    for key, value in scan_result.items():
+        if key != "__files__":
+            ret = _summarize(scan_result=value)
+            error += ret[0]
+            dir_count += ret[1]
+            file_count += ret[2]
+
+    return error, dir_count, file_count
+
+
+def start(*, dir: str, config: dict[str, Any]):
+    root = _os.path.expanduser(dir)
+    max_workers = config.get("max_workers", _MAX_WORKERS)
+    scan_hidden_dirs = config.get("scan_hidden_dirs", _SCAN_HIDDEN_DIRS)
+    scan_hidden_files = config.get("scan_hidden_files", _SCAN_HIDDEN_FILES)
+    
+    scan_result = _scan_directory(
+        root_dir=root, 
+        max_workers=max_workers,
+        scan_hidden_dirs=scan_hidden_dirs,
+        scan_hidden_files=scan_hidden_files,
+    )
+
+    print("✍️ Writing to file... ", end="", flush=True)
+    _os.makedirs("outputs", exist_ok=True)
+    with open("outputs/file_structure.json", "w") as fh:
+        _json.dump(scan_result, fh, indent=4)
+    print("✅")
+
+    if config.get("summarize"):
+        print("🔍 Summarizing... ", end="", flush=True)
+        errors, total_dirs, total_files = _summarize(scan_result=scan_result)
+        print("✅")
+
+        print("-------------- Summary --------------")
+        print(f"Scanned: '{str(root)}'")
+        print(" - Hidden dirs:", "✅" * scan_hidden_dirs or "❌")
+        print(" - Hidden files:", "✅" * scan_hidden_files or "❌")
+        print(f"Workers: {max_workers}")
+        print(f"Total dirs: {total_dirs:,}")
+        print(f"Total files: {total_files:,}")
+        print(f"Failed scans: {errors:,}")
+        print("-------------- Summary --------------")
