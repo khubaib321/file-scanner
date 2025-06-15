21722577258eb760e8274afafd7277d3e236be30
khubaib321 <khubaib.afzal11@gmail.com>
2025-06-15 06:05:47 +0200

    Ignore dirs config

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import src.scanner as _scanner
 
 
 def main():
+    ignore_dirs = set(
+        # "tmp",
+        # "bin", 
+        # "sbin", 
+        # "venv", 
+        # "dist",
+        # "Volumes",
+        # "__pycache__",
+        # "virtualenvs",
+        # "node_modules",
+    )
     _scanner.start(
         dir="~",
         config={
             "summarize": True,
-            "max_workers": 24,
+            "max_workers": 12,
+            "ignore_dirs": ignore_dirs,
             "scan_hidden_dirs": True,
             "scan_hidden_files": True,
         },
--- src/scanner.py
+++ src/scanner.py
@@ -9,23 +9,18 @@ from typing import Any
 from . import _helpers
 
 _MAX_WORKERS = 48
-_IGNORE_DIRS = {
-    # "tmp",
-    # "bin", 
-    # "sbin", 
-    # "venv", 
-    # "dist",
-    # "Volumes",
-    # "__pycache__",
-    # "virtualenvs",
-    # "node_modules",
-}
+_IGNORE_DIRS = set()
 _SCAN_HIDDEN_DIRS = True
 _SCAN_HIDDEN_FILES = True
 
 
-def _should_ignore_dir(path: str, name: str, scan_hidden: bool) -> bool:
-    return name in _IGNORE_DIRS or (not scan_hidden and name.startswith('.'))
+def _should_ignore_dir(path: str, name: str, ignore_dirs: set[str], scan_hidden: bool) -> bool:
+    return (
+        name in ignore_dirs or 
+        path in ignore_dirs or (
+            not scan_hidden and name.startswith('.')
+        )
+    )
 
 
 def _should_ignore_file(name: str, scan_hidden: bool) -> bool:
@@ -44,6 +39,7 @@ def _worker(work_q: _q.Queue):
             break
 
         params["bucket"]["__files__"] = []
+        ignore_dirs = params["ignore_dirs"]
         scan_hidden_dirs = params["scan_hidden_dirs"]
         scan_hidden_files = params["scan_hidden_files"]
 
@@ -58,7 +54,7 @@ def _worker(work_q: _q.Queue):
 
                     elif (
                         entry.is_dir(follow_symlinks=False)
-                        and not _should_ignore_dir(entry.path, entry.name, scan_hidden_dirs)
+                        and not _should_ignore_dir(entry.path, entry.name, ignore_dirs, scan_hidden_dirs)
                     ):
 
                         sub_bucket = {}
@@ -66,6 +62,7 @@ def _worker(work_q: _q.Queue):
                         work_q.put({
                             "path": entry.path,
                             "bucket": sub_bucket,
+                            "ignore_dirs": ignore_dirs,
                             "scan_hidden_dirs": scan_hidden_dirs,
                             "scan_hidden_files": scan_hidden_files,
                         })
@@ -82,6 +79,7 @@ def _scan_directory(
     *, 
     root_dir: str, 
     max_workers: int, 
+    ignore_dirs: set[str],
     scan_hidden_dirs: bool, 
     scan_hidden_files: bool
 ) -> dict:
@@ -95,6 +93,7 @@ def _scan_directory(
         {
             "path": str(root_path),
             "bucket": result,
+            "ignore_dirs": ignore_dirs,
             "scan_hidden_dirs": scan_hidden_dirs,
             "scan_hidden_files": scan_hidden_files,
         },
@@ -139,24 +138,26 @@ def _summarize(*, scan_result: dict) -> tuple[int, int, int]:
 def start(*, dir: str, config: dict[str, Any]):
     root = _os.path.expanduser(dir)
     max_workers = config.get("max_workers", _MAX_WORKERS)
+    ignore_dirs = config.get("ignore_dirs", _IGNORE_DIRS)
     scan_hidden_dirs = config.get("scan_hidden_dirs", _SCAN_HIDDEN_DIRS)
     scan_hidden_files = config.get("scan_hidden_files", _SCAN_HIDDEN_FILES)
     
     scan_result = _scan_directory(
         root_dir=root, 
         max_workers=max_workers,
+        ignore_dirs=ignore_dirs,
         scan_hidden_dirs=scan_hidden_dirs,
         scan_hidden_files=scan_hidden_files,
     )
 
-    print("✍️ Writing to file... ", end="", flush=True)
+    print("✍️   Writing to file... ", end="", flush=True)
     _os.makedirs("outputs", exist_ok=True)
     with open("outputs/file_structure.json", "w") as fh:
         _json.dump(scan_result, fh, indent=4)
     print("✅")
 
     if config.get("summarize"):
-        print("🔍 Summarizing... ", end="", flush=True)
+        print("🔍   Summarizing... ", end="", flush=True)
         errors, total_dirs, total_files = _summarize(scan_result=scan_result)
         print("✅")
 
